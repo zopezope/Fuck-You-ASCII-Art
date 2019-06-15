3024a68dc57c949ba95366b46a4fdddf36e404d8
zopezope <51864916+zopezope@users.noreply.github.com>
2019-06-15 18:57:56 -0500

    The original C++ file
    Got bored, wanted to practice

---
+++ fuck-you.cpp
@@ -0,0 +1,21 @@
+//outputs fuck you in ASCII art
+#include <iostream>
+
+int main() {
+    std::cout << "FFFFF  U   U   CCC   K   K\n";
+    std::cout << "F      U   U  C   C  K  K \n";
+    std::cout << "F      U   U  C      K K  \n";
+    std::cout << "FFF    U   U  C      KK   \n";
+    std::cout << "F      U   U  C      K K  \n";
+    std::cout << "F      U   U  C   C  K  K \n";
+    std::cout << "F       UUU    CCC   K   K\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "Y   Y   OOO   U   U\n";
+    std::cout << " Y Y   O   O  U   U\n";
+    std::cout << "  Y    O   O  U   U\n";
+    std::cout << "  Y    O   O  U   U\n";
+    std::cout << "  Y    O   O  U   U\n";
+    std::cout << "  Y    O   O  U   U\n";
+    std::cout << "  Y     OOO    UUU \n";
+}
